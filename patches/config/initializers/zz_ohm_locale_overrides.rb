06fc2c7c5791c5d2d739ec5dfeb4cc4068de501e
Rub21 <rub2106@gmail.com>
2026-08-01 10:29:18 -0500

    Remove overlay files accidentally copied into patches/

--- patches/config/initializers/zz_ohm_locale_overrides.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-
-# Load OHM locale overrides last so they win over upstream strings.
-Rails.application.config.i18n.load_path += Rails.root.glob("config/locales/overrides/*.yml")
--- patches/db/migrate/20240401010024_change_column_user_consider_pd_default.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class ChangeColumnUserConsiderPdDefault < ActiveRecord::Migration[7.0]
-  def change
-    change_column_default :users, "consider_pd", :from => false, :to => true
-  end
-end
